ee7801a480d6fd44f9c93719b920204a89570d0d
FioriMartins <caiofiorigamer1234@outlook.com>
2024-10-11 18:16:07 -0300

    Criar categoria feito; Fazer o resto

--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -3,8 +3,14 @@ const express = require('express')
 const cors = require('cors')
 const initModels = require('./init-models').initModels
 
-const sequelize = new Sequelize('feiradatroca', 'fiori', 'Julialinda1!', {
-    host: "localhost",
+require('dotenv').config();
+
+const app = express()
+const PORT = process.env.DB_PORT
+const HOST = process.env.DB_HOST
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: HOST,
     dialect: "mysql"
 })
 
@@ -16,9 +22,6 @@ sequelize.authenticate().then(() => {
 
 const models = initModels(sequelize)
 
-const app = express()
-const PORT = 3000
-
 app.use(cors())
 app.use(express.json())
 
@@ -27,11 +30,29 @@ app.get('/admin', async (req, res) => {
     res.json(admin)
 })
 
+app.get('/categoria', async (req, res) => {
+    const categorias = await models.categoria.findAll()
+    res.json(categorias)
+})
+
+
+// endpoint method post para receber
+
+app.post('/categoria', async (req, res) => {
+    const {nome, precodef} = req.body
+
+    try {
+        const novosDados = await models.categoria.create({
+            nome: nome,
+            precodef: precodef
+        })
+        res.status(201).json(novosDados)
+    } catch (e) {
+        res.status(500).json({e: 'Erro ao criar usuário'})
+        console.log(e)
+    }
+})
+
 app.listen(PORT, () => {
     console.log('Servidor rodando')
 })
-
-module.exports = {
-    Sequelize: Sequelize,
-    sequelize: sequelize
-}
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from 'react'
+import { React, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formDados, setFormDados] = useState({
+    nome: '',
+    precodef: ''
+  })
+
+  const handleChange = (e) => {
+    const {name, value} = e.target 
+    setFormDados({
+      ...formDados,
+      [name]: value,
+    })
+    console.log(formDados)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('http://#/categoria', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formDados)
+      })
+
+      if (response.ok) {
+        alert('Categoria registrada com sucesso!')
+      }
+    } catch (e) {
+      alert('deu erro')
+      console.error('Erro ao enviar os dados: ', error)
+    }
+  }
 
   return (
     <>
       <h1>Feira da Troca</h1>   
-      <input />
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleChange} name="nome" />
+        <input type='number' onChange={handleChange} name='precodef'/>
+        <button type='submit'>Enviar</button>
+      </form>
     </>
   )
 }
